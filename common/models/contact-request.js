9381f4cbb28f329ad2cc1af8f0c1e1144ea7cbcd
Ernest Gayyed <egayyed@isbx.com>
2016-12-14 02:05:05 -0800

    added contact request

--- common/models/contact-request.js
+++ common/models/contact-request.js
@@ -1,9 +1,7 @@
 module.exports = function(ContactRequest) {
 	ContactRequest.observe('before save', function(context, next) {
 		if (context.isNewInstance) {
-			console.log(context.instance.req);
-			context.instance.fromAccountId = 1;
-			context.instance.requestDate = new Date();
+			context.instance.createdDate = new Date();
 		}
 
 		next();
---
+++ common/models/message.js
@@ -0,0 +1,6 @@
+/**
+ * Created by egayyed on 13/12/2016.
+ */
+module.exports = function(Message) {
+
+}
