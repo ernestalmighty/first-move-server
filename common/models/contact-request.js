da22ec3e405913dc31a32a890ee1d945cd7e4c9f
Ernest Gayyed <egayyed@isbx.com>
2016-12-15 00:44:52 -0800

    add socket io implementation

--- common/models/contact-request.js
+++ common/models/contact-request.js
@@ -1,9 +1,19 @@
+var pubsub = require('../../server/pubsub.js');
 module.exports = function(ContactRequest) {
 	ContactRequest.observe('before save', function(context, next) {
 		if (context.isNewInstance) {
 			context.instance.createdDate = new Date();
 		}
 
+		next();
+	});
+
+	ContactRequest.observe('after save', function(context, next) {
+		var socket = ContactRequest.app.io;
+		pubsub.publish(socket, {
+			toAccount : context.instance.toAccountId,
+			data: context.instance
+		});
 		next();
 	});
 };
---
+++ server/pubsub.js
@@ -0,0 +1,36 @@
+/**
+ * Created by egayyed on 14/12/2016.
+ */
+'use strict';
+//Writing pubsub module for socket.io
+module.exports = {
+    //Publishing a event..
+    publish: function(socket, options ){
+        if(options){
+            var data = options.data;
+            var toAccountId = "contact_request";
+
+            socket.emit(toAccountId, data);
+
+        }else{
+            throw 'Error: Option must be an object type';
+        }
+    }, //End Publish..
+
+    isEmpty:function (obj) {
+        var hasOwnProperty = Object.prototype.hasOwnProperty;
+        // null and undefined are "empty"
+        if (obj == null) return true;
+        // Assume if it has a length property with a non-zero value
+        // that that property is correct.
+        if (obj.length > 0)    return false;
+        if (obj.length === 0)  return true;
+        // Otherwise, does it have any properties of its own?
+        // Note that this doesn't handle
+        // toString and valueOf enumeration bugs in IE < 9
+        for (var key in obj) {
+            if (this.hasOwnProperty.call(obj, key)) return false;
+        }
+        return true;
+    } //isEmpty function..
+}
